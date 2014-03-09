c17b41626b0963eeac322de3160f10fdc12567f0
Max Howell <mxcl@me.com>
2014-03-08 19:42:35 -0600

    NSString.split(@"") works
    It is logical that splitting by nothing will split by the smallest possible unit, ie. nothing, so all characters are split out into an array.
    
    Though, no other framework does this. It's often something I've expected and sometimes: needed.

--- NSString+YOLO.m
+++ NSString+YOLO.m
@@ -5,7 +5,14 @@
 
 - (NSArray *(^)(NSString *))split {
     return ^NSArray *(NSString *separator){
-        return [self componentsSeparatedByString:separator];
+        if (separator.length == 0) {
+            const NSUInteger N = self.length;
+            id chars[N];
+            for (NSUInteger x = 0; x < N; ++x)
+                chars[x] = [self substringWithRange:NSMakeRange(x, 1)];
+            return [NSArray arrayWithObjects:chars count:N];
+        } else
+            return [self componentsSeparatedByString:separator];
     };
 }
 
