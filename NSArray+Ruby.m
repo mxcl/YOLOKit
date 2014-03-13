6cbaeea8805c80d390e4d12143a5fcf1fada58fa
Max Howell <mxcl@me.com>
2014-03-13 15:03:20 -0500

    NSArray.any can take a class parameter

--- NSArray+Ruby.m
+++ NSArray+Ruby.m
@@ -262,7 +262,17 @@ extern NSMethodSignature *YOLOMS(id);
 }
 
 - (BOOL(^)(id))any {
-    return ^(BOOL(^block)(id o)){
+    return ^(id arg){
+        BOOL (^block)(id o) = nil;
+
+        if (arg == NSString.class /*or segfaults!*/ || class_isMetaClass(object_getClass(arg))) {
+            Class cls = arg;
+            block = ^(id o){
+                return [o isKindOfClass:cls];
+            };
+        } else
+            block = arg;
+
         for (id o in self)
             if (block(o))
                 return YES;
