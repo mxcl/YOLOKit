a1a0abc4dc656efe93662b63419e57333e4a3411
Max Howell <mxcl@me.com>
2014-04-16 14:21:21 -0500

    sortBy handles return of primitives

--- NSArray+Ruby.m
+++ NSArray+Ruby.m
@@ -214,18 +214,51 @@ extern NSMethodSignature *YOLOMS(id);
 - (NSArray *(^)(id))sortBy {
     // TODO pass an array to sort by multiple selectors (use multiple sort descriptors)
 
-    return ^(id blockOrKey) {
+    return ^NSArray *(id blockOrKey) {
         if ([blockOrKey isKindOfClass:[NSString class]])
             blockOrKey = ^(id o){
                 return [o valueForKey:blockOrKey];
             };
 
-        // FIXME inefficient
-        NSArray *keys = self.map(blockOrKey);
-        return keys.sort.map(^(id key){
-            NSUInteger ii = [keys indexOfObject:key];
-            return self[ii];
-        });
+        #define foo(TYPE) { TYPE (^block)(id) = blockOrKey;\
+            return [self sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {\
+                TYPE a = block(obj1);\
+                TYPE b = block(obj2);\
+                return (NSComparisonResult)(a - b);\
+            }]; }
+
+        const char rt = YOLOMS(blockOrKey).methodReturnType[0];
+        switch (rt){
+            case 'i':
+            case 'l':
+            case 'c':
+            case 's':
+                foo(long);
+            case 'q':
+                foo(long long);
+            case 'I':
+            case 'L':
+            case 'C':
+            case 'S':
+                foo(unsigned long);
+            case 'Q':
+                foo(unsigned long long);
+            case 'f':
+            case 'd':
+                foo(double);
+            case '@': {
+                id (^block)(id) = blockOrKey;
+                return [self sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+                    obj1 = block(obj1);
+                    obj2 = block(obj2);
+                    return [obj1 compare:obj2];
+                }];
+                break;
+            }
+            default:
+                @throw @"Cannot handle sortBy block return type. Please fork and fix!";
+                return nil;
+        }
     };
 }
 
