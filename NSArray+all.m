7d943270c2b37425b8ab4b23f35589c90cc8dcc3
Max Howell <mxcl@me.com>
2014-05-31 11:20:16 -0500

    Allow `all`’s block to transform to primitives

--- NSArray+all.m
+++ NSArray+all.m
@@ -12,8 +12,26 @@
             block = ^(id o){
                 return [o isKindOfClass:cls];
             };
-        } else
-            block = arg;
+        } else {
+            switch ([YOLOMS(arg) getArgumentTypeAtIndex:1][0]) {
+                case 'c': { block = ^(id o){ return ((BOOL(^)(char))arg)([o charValue]); }; break; }
+                case 'i': { block = ^(id o){ return ((BOOL(^)(int))arg)([o intValue]); }; break; }
+                case 's': { block = ^(id o){ return ((BOOL(^)(short))arg)([o shortValue]); }; break; }
+                case 'l': { block = ^(id o){ return ((BOOL(^)(long))arg)([o longValue]); }; break; }
+                case 'q': { block = ^(id o){ return ((BOOL(^)(long long))arg)([o longLongValue]); }; break; }
+                case 'C': { block = ^(id o){ return ((BOOL(^)(unsigned char))arg)([o unsignedCharValue]); }; break; }
+                case 'I': { block = ^(id o){ return ((BOOL(^)(unsigned))arg)([o unsignedIntValue]); }; break; }
+                case 'S': { block = ^(id o){ return ((BOOL(^)(unsigned short))arg)([o unsignedShortValue]); }; break; }
+                case 'L': { block = ^(id o){ return ((BOOL(^)(unsigned long))arg)([o unsignedLongValue]); }; break; }
+                case 'Q': { block = ^(id o){ return ((BOOL(^)(unsigned long long))arg)([o unsignedLongLongValue]); }; break; }
+                case 'f': { block = ^(id o){ return ((BOOL(^)(float))arg)([o floatValue]); }; break; }
+                case 'd': { block = ^(id o){ return ((BOOL(^)(double))arg)([o doubleValue]); }; break; }
+                case 'B': { block = ^(id o){ return ((BOOL(^)(BOOL))arg)([o boolValue]); }; break; }
+                default:
+                    block = arg;
+                    break;
+            }
+        }
 
         for (id o in self)
             if (!block(o))
