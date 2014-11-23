bbb5eb2e38080380c4e2b68ecd4ed75a4238dee5
Max Howell <mxcl@me.com>
2014-11-23 12:14:30 -0500

    Don’t crash if sample is called on @[]
    Fixes #12
    
    Tag 11.0.1

--- NSArray+sample.m
+++ NSArray+sample.m
@@ -3,8 +3,10 @@
 
 @implementation NSArray (YOLO)
 
-- (id)sample {   
-    return self[arc4random_uniform((u_int32_t)self.count)];
+- (id)sample {
+    return self.count == 0
+        ? nil
+        : self[arc4random_uniform((u_int32_t)self.count)];
 }
 
 @end
