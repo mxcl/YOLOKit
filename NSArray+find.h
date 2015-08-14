cdf5fab05918a4d9b57483b9018fdb180b4fc364
Max Howell <mxcl@me.com>
2015-08-13 21:01:54 -0700

    Correct spelling typo

--- NSArray+find.h
+++ NSArray+find.h
@@ -3,7 +3,7 @@
 @interface NSArray (YOLOFind)
 
 /**
- Passes each entry in the arry to the given block, returning the first
+ Passes each entry in the array to the given block, returning the first
  element for which block is not `NO`. If no object matches, returns
  `nil`.
 
